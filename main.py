745436cb65a68b66485b8499b90c7ef3104b4dca
FHU-yezi <yehaowei20060411@qq.com>
2021-11-06 07:39:29 +0800

    feat：更换框架，增加新功能

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from time import sleep
+from JianshuResearchTools.collection import GetCollectionArticlesInfo
+from JianshuResearchTools.convert import (ArticleSlugToArticleUrl,
+                                          ArticleUrlToArticleUrlScheme,
+                                          UserSlugToUserUrl)
+from pandas import DataFrame
+from pywebio import pin, start_server
+from pywebio.input import *
+from pywebio.output import *
 
-import JianshuResearchTools as jrt
-import pandas as pd
-import streamlit as st
-
-__version__ = "0.3.0"
+__version__ = "1.0.0"
 
 collections = {
     "简友广场": "https://www.jianshu.com/c/7ecac177f5a8", 
@@ -12,124 +15,116 @@ collections = {
     "想法": "https://www.jianshu.com/c/qQB2Zn"
 }
 
+def CheckData():
+    all_data_right = True
+    if not 1 <= pin.pin["likes_limit"] <= 10:
+        toast("点赞数上限必须在 1 到 5 之间", color="error")
+        all_data_right = False
+    if not 1 <= pin.pin["comments_limit"] <= 10:
+        toast("评论数上限必须在 1 到 5 之间", color="error")
+        all_data_right = False
+    if not 20 <= pin.pin["max_result_count"] <= 100:
+        toast("结果数量必须在 20 到 100 之间", color="error")
+        all_data_right = False
+    if not pin.pin["chosen_collections"]:
+        toast("请至少选择一个专题", color="error")
+        all_data_right = False
+    if not (pin.pin["fp_amount_limit"] == 0 or 0.1 <= pin.pin["fp_amount_limit"] <= 30.0):
+        toast("文章获钻量限制必须在 0.1 到 30.0 之间", color="error")
+        all_data_right = False
+    return all_data_right
+        
+def GetProcessedData():
+    chosen_collections_urls = []
+    for chosen_collection in pin.pin["chosen_collections"]:
+            chosen_collections_urls.append(collections[chosen_collection])
+    raw_data = []
+    for collection_url in chosen_collections_urls:
+        for page in range(1, 7):
+            raw_data.extend(GetCollectionArticlesInfo(collection_url, page))  # 默认获取 7 页
+    df = DataFrame(raw_data)
+    
+    df = df[df["likes_count"] <= pin.pin["likes_limit"]]  # 根据点赞数筛选
+    df = df[df["likes_count"] <= pin.pin["comments_limit"]]  # 根据评论数筛选
+    if "仅展示可评论的文章" in pin.pin["additional_features"]:
+        df = df[df["commentable"] == True]  # 筛选允许评论的文章
+    if "不展示付费文章" in pin.pin["additional_features"]:
+        df = df[df["paid"] == False]  # 筛选不需要付费的文章
+    if pin.pin["fp_amount_limit"] != 0:
+        df = df[df["total_fp_amount"] <= pin.pin["fp_amount_limit"]]  # 根据获钻量筛选
 
-def GetCollectionArticlesDataFrame(collection_url, pages):
-    df = pd.DataFrame(columns=["title", "nid", "likes_count", "time", "commentable", 
-                               "paid", "comments_count", "fp_amount", "rewards_count", "slug"])
-    for page in range(pages):
-        page += 1  # 还记得 range 从 0 开始不？
-        result = jrt.GetCollectionArticlesList(collection_url, page)
-        for article in result:
-            article["time"] = jrt.StrToDatetime(article["time"])  # 处理日期字符串，方便比较时间
-            article["likes_count"] = int(article["likes_count"])
-            article["comments_count"] = int(article["comments_count"])
-        for article in result:
-            df.loc[len(df.index)] = article  # 插入数据
+    df = df[:pin.pin["max_result_count"]]  # 切片
     return df
 
-def FilterArticlesByLikesCount(article_df, likes_limit):
-    filtered_df = article_df[article_df["likes_count"] <= likes_limit]
-    return filtered_df
-
-def FilterArticlesByCommentsCount(article_df, comments_limit):
-    filtered_df = article_df[article_df["likes_count"] <= likes_limit]
-    return filtered_df
-
-def FilterCommentableOnly(article_df):
-    filtered_df = article_df[article_df["commentable"] == True]
-    return filtered_df
-
-def FilterFreeOnly(article_df):
-    filtered_df = article_df[article_df["paid"] == False]
-    return filtered_df
-
-def FilterByFPAmount(article_df, fp_limit):
-    filtered_df = article_df[article_df["fp_amount"] <= fp_limit]
-    return filtered_df
-
-st.title("简书消零派辅助工具")
-
-info = st.empty()
-info.markdown("""**消灭零评论，留下爱与光。**
-
-本工具为辅助简书消零派寻找符合条件的文章而开发。
-                
-请展开侧边栏，调整设置并获取文章列表。
-                
-工作原理：在您选定的专题中查找新发布且赞、评少于一定数量的文章，进行处理后展示到页面上。
-""")
-
-with st.sidebar.form("参数设定"):
-    submitted = st.form_submit_button("提交并查找")
-    
-    comments_limit = st.slider("评论数量上限", min_value=1, max_value=5)
-    likes_limit = st.slider("点赞数量上限", min_value=1, max_value=5)    
-    chosen_collections = st.multiselect("专题选择", options=list(collections.keys()))
-    max_result_count = st.number_input("输出结果数量", min_value=20, max_value=100)
-
-with st.sidebar.beta_expander("展开额外选项"):
-    # ! 这个功能目前只是摆设，还没有实现，所以不展示出来
-    # enable_title_stopword = st.checkbox("开启标题停用词")
-    # if enable_title_stopword == True:
-    #     title_stopword = st.text_input("标题停用词", help="请以英文逗号分隔")
-    # else:
-    #     title_stopword = None
-    enable_url_scheme = st.checkbox("开启 URL Scheme 跳转", help="开启后文章详情中会出现一个跳转链接，可以直接跳转到简书 App 中阅读对应文章")
-    commentable_only = st.checkbox("仅展示可以评论的文章", value=True)
-    free_only = st.checkbox("不展示需要付费阅读的文章")
-    enable_fp_amount_limit = st.checkbox("启用文章获钻数量限制")
-    if enable_fp_amount_limit == True:
-        fp_amount_limit = st.number_input("请输入最大获钻量", value=0.10, min_value=0.10, max_value=30.00)
-    else:
-        fp_amount_limit = None
-st.sidebar.write("版本：V", __version__)
-
-st.sidebar.write("Powered By JRT")
-
-if submitted == True:
-    if chosen_collections == []:
-        st.warning("请选择专题")
-        st.stop()
-    else:
-        info.empty()  # 隐藏提示语
-        chosen_collections_urls = []
-        for chosen_collection in chosen_collections:
-            chosen_collections_urls.append(collections[chosen_collection])
+def ShowResult(df):
+    with use_scope("output", if_exist="remove"):  # 如果 scope 已存在，将其替换
+        put_markdown("---")  # 分割线
         
-    article_df = pd.DataFrame(columns=["title", "nid", "likes_count", "time", "commentable", 
-                               "paid", "comments_count", "fp_amount", "rewards_count", "slug", 
-                               "from_collection"])
-    for chosen_collection_url in chosen_collections_urls:
-        result = GetCollectionArticlesDataFrame(chosen_collection_url, 7)  # 默认获取 7 页
-        for index in result.index:
-            article = result.loc[index]
-            for name, url in collections.items():  # 通过值反查字典键
-                if url == chosen_collection_url:
-                    article["from_collection"] = name
-            article_df.loc[len(article_df.index)] = article
+        for index, item in df.iterrows():
+            content = f"""链接：{ArticleSlugToArticleUrl(item.aslug)}
+            作者：[{item.user["name"]}]({UserSlugToUserUrl(item.user["uslug"])})
+            发布时间：{item.release_time.strftime(r"%Y-%m-%d %X")}
+            阅读量：{item.views_count}
+            点赞数：{item.likes_count}
+            评论数：{item.comments_count}
+            获钻量：{item.total_fp_amount}
+
+            摘要：
+
+            {item.summary}
+            \n
+            """
+            if "开启 URL Scheme 跳转" in pin.pin["additional_features"]:  # 开启了 URL Scheme 跳转
+                put_collapse(f"{item.title}", [
+                    put_markdown(content, lstrip=True), 
+                    put_link("点击跳转到简书 App（手机端）", url=ArticleUrlToArticleUrlScheme(ArticleSlugToArticleUrl(item.aslug)))
+                    ])
+            else:
+                put_collapse(f"文章标题：{item.title}", put_markdown(content, lstrip=True))
     
-    filtered_df = FilterArticlesByLikesCount(article_df, likes_limit)
-    filtered_df = FilterArticlesByCommentsCount(filtered_df, comments_limit)
-    if commentable_only == True:
-        filtered_df = FilterCommentableOnly(filtered_df)
-    if free_only == True:
-        filtered_df = FilterFreeOnly(filtered_df)
-    if enable_fp_amount_limit == True:
-        filtered_df = FilterByFPAmount(filtered_df, fp_amount_limit)
+def main_logic():    
+    if CheckData() == False:
+        return  # 有数据填写错误，不运行后续逻辑
+    df = GetProcessedData()
+    toast("数据获取成功！", color="success")
+    ShowResult(df)
     
-    cutted_df = filtered_df[0:max_result_count]
+def main_show():
+    """简书消零派辅助工具
     
-    info.subheader("文章列表")  # 展示文章前显示标题
-    count = 1
-    for index in cutted_df.index:
-        article = cutted_df.loc[index]
-        with st.beta_expander("【" + str(count) + "】" + article["title"]):
-            article_url = "https://www.jianshu.com/p/" + article["slug"]
-            if enable_url_scheme == True:
-                st.markdown("[点击在 App 中阅读文章](" + jrt.GetArticlePageURLScheme(article_url) + ")")
-            st.write("文章链接：" + article_url)
-            st.write("发布时间：" + str(article["time"]))
-            st.write("来源：", article["from_collection"])
-            st.write("点赞数：" + str(article["likes_count"]))
-            st.write("评论数：" + str(article["comments_count"]))
-        count += 1
+    消灭零评论，留下爱与光。
+    """
+    
+    put_markdown("# 简书消零派辅助工具")
+    
+    put_markdown(f"""**消灭零评论，留下爱与光。**
+
+    本工具为辅助简书消零派寻找符合条件的文章而开发。
+                    
+    请展开侧边栏，调整设置并获取文章列表。
+                    
+    工作原理：在您选定的专题中查找新发布且赞、评少于一定数量的文章，进行处理后展示到页面上。
+    
+    Made with [JRT](https://github.com/FHU-yezi/JianshuResearchTools) and ♥
+    Version：{__version__}
+    """, lstrip=True)  # 去除无用的空格
+    
+    put_markdown("---")  # 分割线
+    
+    put_row([
+        put_column([
+            pin.put_input("likes_limit", label="点赞数上限", type=NUMBER, value=3, help_text="介于 1 到 10 之间"), None,
+            pin.put_input("comments_limit", label="评论数上限", type=NUMBER, value=3, help_text="介于 1 到 10 之间"), None, 
+            pin.put_input("max_result_count", label="结果数量", type=NUMBER, value=20, help_text="介于 20 到 100 之间")
+        ]), None, 
+        put_column([
+            pin.put_checkbox("chosen_collections", label="专题选择", options=collections.keys()), 
+            pin.put_checkbox("additional_features", label="高级选项", options=["开启 URL Scheme 跳转", "仅展示可评论的文章", 
+                                            "不展示付费文章"]), 
+            pin.put_input("fp_amount_limit", type=FLOAT, label="文章获钻量限制", value=0.0, help_text="介于 0.1 到 30.0 之间，0 为关闭")
+        ])
+    ], size=r"3fr 1fr 3fr")
+
+    put_button("提交", color="success", onclick=main_logic)
+
+start_server(main_show, port=8601)
